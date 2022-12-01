ead306ce3144929608cce19e711849ffb2c34413
hvlopez <hectorvmlopez@gmail.com>
2022-11-30 23:44:58 -0500

    feat(master): adding nestjs backend template

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
   // This tells ESLint to load the config from the package `eslint-config-custom`
   extends: ["custom"],
   settings: {
-    next: {
+    demo: {
+      rootDir: ["apps/*/"],
+    },
+    backend: {
       rootDir: ["apps/*/"],
     },
   },
---
+++ apps/backend/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  ...require('eslint-config-custom/index'),
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'], // !!! ne
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
